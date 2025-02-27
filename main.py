5b9314b86ebd5660b63a1a72830fed0105d1d9df
niragui <ignacioiragui2@gmail.com>
2025-02-26 22:32:07 -0300

    feat: Add Relative Paths

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from src.script_handler import ScriptHandler
 
+import traceback
+import os
+
+THIS_FOLDER = os.path.dirname(__file__)
+LOG_FILE = os.path.join(THIS_FOLDER, "log.txt")
+
 
 if __name__ == "__main__":
-    handler = ScriptHandler("scripts.json")
-    handler.check_scripts()
+    try:
+        scripts_file = os.path.join(THIS_FOLDER, "scripts.json")
+        handler = ScriptHandler(scripts_file)
+        handler.check_scripts()
+    except:
+        error = traceback.format_exc()
+        with open(LOG_FILE, "w") as f:
+            f.write(error)
